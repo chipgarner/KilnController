3b8c6ea1f527fb2268c6063f759c9761d94228bf
James Kirikland Garner <chip@garnertotalenergy.com>
2023-01-07 20:19:55 -0800

    More spi...

--- lib/TempSensors.py
+++ lib/TempSensors.py
@@ -335,7 +335,7 @@ class Max31856(TempSensorReal):
         TempSensorReal.__init__(self)
         log.info("thermocouple MAX31856")
         import adafruit_max31856
-        self.thermocouple = adafruit_max31856.MAX31856(config.spi_cs_56, self.cs,
+        self.thermocouple = adafruit_max31856.MAX31856(self.spi, config.spi_cs_56,
                                         thermocouple_type=config.thermocouple_type)
 
         if (config.ac_freq_50hz == True):
